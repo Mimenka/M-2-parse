3be942f737b090744d5bb48920e87a348b7526f5
Айтурган Кадыралиева <ajturgankadyralieva@Ajturgans-MacBook-Pro.local>
2024-05-03 18:34:18 +0600

    hw is done

--- main.py
+++ main.py
@@ -1,28 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
+
+
 base_url = 'https://dominopizza.ru/'
 response = requests.get(base_url, verify=False)
-soup = BeautifulSoup(response.text, 'html')
-zakuski = soup.find('div',{'id': 'zakuski'})
-zakuski_col = zakuski.findAll('div',{'class': 'col'})
+soup = BeautifulSoup(response.text, 'html.parser')
+
+zakuski = soup.find('div', {'id': 'zakuski'}).find_all(
+    'div', {'class': 'col'})
+
 zakuski_list = []
 
-for zakuski in zakuski_col:
-    zakuski_content = zakuski.a.div.find('div',{'class':'product-card-content'})
-    zakuski_content_name = zakuski_content.find('div', {'class': 'product-name'})
-    zakuski_content_description = zakuski_content.find('div', {'class': 'description-container'})
-    zakuski_content_price = zakuski_content.find('div', {'class': 'price'})
-    # zakuski_pic = zakuski.a.div.find('div',{'class': 'product_picture'})
+for zak in zakuski:
+    zakuski_content = zak.a.div.find('div', {'class': 'product-card-content'})
+    zakuski_name = zakuski_content.find('div', {'class': 'product-name'})
+    zakuski_descrip = zakuski_content.find('div', {'class': 'description-container'})
+    zakuski_price = zakuski_content.find('div', {'class': 'price'})
+    zakuski_picture = zak.a.div.find('div', {'class': 'product-picture'})
+
+
     zakuski_list.append({
-        'name': zakuski_content_name.get_text(), 
-        'descriptons': zakuski_content_description.get_text(),
-        'price': zakuski_content_price.get_text(),
-        # 'picture': zakuski_pic.get_text()
+        'name': zakuski_name.get_text(),
+        'descrip': zakuski_descrip.get_text(),
+        'price': zakuski_price.get_text(),
+        'picture_url': zakuski_picture.picture.img.get('src')
     })
 print('Закуски')
-for zakusk in zakuski_list:
+for zak in zakuski_list:
     print('------------------------------')
-    print(f'Название: {zakusk["name"]}')
-    print(f'Состав: {zakusk["descriptons"]}')
-    print(f'Цена: {zakusk["price"]}')
+    print(f'Название: {zak["name"]}')
+    print(f'Состав: {zak["descrip"]}')
+    print(f'Цена: {zak["price"]}')
+    print(f'Ссылка на картину: {zak["picture_url"]}')
     print('------------------------------')
